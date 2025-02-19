4ecb7c4e3bbc6ea79a489a457fbfcb613981a193
Sam <jhordart@gmail.com>
2025-02-19 10:58:58 -0500

    commit n ???? cambios en dockerfile y otros

--- CLIENT/react-router.config.ts
+++ CLIENT/react-router.config.ts
@@ -1,8 +1,18 @@
 import type { Config } from "@react-router/dev/config";
-
+import { writeFile } from "fs/promises";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  buildDirectory: "build/client",
+  async buildEnd({ buildManifest }) {
+    const manifest: any = buildManifest;
+    if (!manifest.publicPath) {
+      manifest.publicPath = "/";
+    }
+    if (!manifest.assetsBuildDirectory) {
+      manifest.assetsBuildDirectory = "build/client";
+    }
+    await writeFile("build/manifest.json", JSON.stringify(manifest, null, 2), "utf-8");
+  },
+
 } satisfies Config;
