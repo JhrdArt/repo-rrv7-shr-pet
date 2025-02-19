bdb17998bb11767ed075090cb60eaad45e289659
Sam <jhordart@gmail.com>
2025-02-19 09:55:31 -0500

    dockerizando

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -2,7 +2,7 @@ import http from "http";
 
 import { app } from "./app.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
